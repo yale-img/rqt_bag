082a1184022e685f1ac5faa51d58dbc38e46fc3b
Sasha Lew <a.lew@yale.edu>
2026-08-31 15:26:00 -0400

    increase timeline responsiveness a la ros-visualization/rqt_bag#177

--- rqt_bag/src/rqt_bag/bag_widget.py
+++ rqt_bag/src/rqt_bag/bag_widget.py
@@ -429,6 +429,7 @@ class BagWidget(QWidget):
         self._timeline.handle_close()
 
     def update_size(self):
+        self._timeline.update_size()
         self._resizeEvent(QResizeEvent(self.size(), self.size()))
 
     def on_mousewheel(self, event):
